90f4f26142d01711780f7c8abfd415d60c751cb9
Moussa Alkaly BADJI <moussabadji079@gmail.com>
2022-08-31 10:12:00 +0000

    feat: :sparkles: permettre au user d'annuler une commande

--- src/components/commands/UserCommands.js
+++ src/components/commands/UserCommands.js
@@ -1,26 +1,62 @@
-import React from 'react'
-import { useWhereDocs } from 'easy-firestore/hooks';
-import { db } from '../../firebase/config';
-import { useAuth } from '../../hooks/useAuth';
-import { Link } from 'react-router-dom';
-import CommandCard from './CommandCard';
+import React from "react";
+import { useWhereDocs } from "easy-firestore/hooks";
+import { db } from "../../firebase/config";
+import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
+import CommandCard from "./CommandCard";
+import { updateDoc } from 'firebase/firestore';
+import { doc } from 'firebase/firestore';
 
 const UserCommands = () => {
-    const { currentUser } = useAuth()
-    const userId = currentUser?.uid
-    const { data: commands, numberOfData: numberOfCommands, dataLoading: commandsLoading } = useWhereDocs(db, 'commands', 'commandedBy', userId)
-    return (
-        <div className=''>
-            <h1 className='border-bottom commands-h1'>Liste de vos commandes</h1>
-            {
-                !commandsLoading ? numberOfCommands !== 0 && commands?.filter(command => command.isConfirmed === true).length !== 0 ? commands?.filter(command => command.isConfirmed === true).map(command => (
-                    <CommandCard key={command.id} {...command} numberOfCommand={command.userCommands.length}>
-                        <button style={{ fontWeight: '600' }} className='btn btn-outline-danger me-2' type="">Annuler la commande</button>
-                    </CommandCard>
-                )) : <p className='p-3'>Pas encore  de commandes</p> : <p className='p-3'>loading...</p>
-            }
-        </div>
-    )
-}
+    const { currentUser } = useAuth();
+    const userId = currentUser?.uid;
+    const {
+        data: commands,
+        numberOfData: numberOfCommands,
+        dataLoading: commandsLoading,
+    } = useWhereDocs(db, "commands", "commandedBy", userId);
 
-export default UserCommands
+    const canceledCommand = (id) => {
+        const selectedCommand = commands.find(command => command.id === id)
+        const isConfirmed = selectedCommand.isConfirmed
+        updateDoc(doc(db, 'commands', id), {
+            isConfirmed: !isConfirmed
+        })
+    }
+
+    return (
+        <div className="">
+            <h1 className="border-bottom commands-h1">Liste de vos commandes</h1>
+            {!commandsLoading ? (
+                numberOfCommands !== 0 &&
+                    commands?.filter((command) => command.isConfirmed === true).length !==
+                    0 ? (
+                    commands
+                        ?.filter((command) => command.isConfirmed === true)
+                        .map((command) => (
+                            <CommandCard
+                                key={command.id}
+                                {...command}
+                                numberOfCommand={command.userCommands.length}
+                            >
+                                <button
+                                    onClick={() => canceledCommand(command.id)}
+                                    style={{ fontWeight: "600" }}
+                                    className="btn btn-outline-danger me-2"
+                                    type=""
+                                >
+                                    Annuler la commande
+                                </button>
+                            </CommandCard>
+                        ))
+                ) : (
+                    <p className="p-3">Pas encore de commandes</p>
+                )
+            ) : (
+                <p className="p-3">loading...</p>
+            )}
+        </div>
+    );
+};
+
+export default UserCommands;
